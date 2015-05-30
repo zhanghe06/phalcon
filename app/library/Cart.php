37d4b92694d6dd195e94d6b59c0f26502b078ee9
Zhang He <zhang_he06@163.com>
2015-05-30 15:13:15 +0800

    优化购物车类

--- app/library/Cart.php
+++ app/library/Cart.php
@@ -12,19 +12,30 @@
  */
 class Cart extends \Phalcon\Di\Injectable
 {
-    private $Prefix = 'cart';
+    private $Prefix;
+    private $uid;
+
+    /**
+     * 初始化购物车
+     * @param $uid
+     * @param string $Prefix
+     */
+    public function __construct($uid, $Prefix='cart')
+    {
+        $this->uid = $uid;
+        $this->Prefix = $Prefix;
+    }
 
     /**
      * 添加物品
-     * @param $uid
      * @param $pid
      * @param int $num
      * @return bool
      */
-    public function Add($uid, $pid, $num = 1)
+    public function Add($pid, $num = 1)
     {
         $redis = $this->di->getShared('redis');
-        $key = "$this->Prefix:$uid:$pid";
+        $key = "$this->Prefix:$this->uid:$pid";
         //判断物品是否存在
         if ($redis->exists($key)) {
             //如果存在，仅增加相应数量
@@ -43,14 +54,13 @@ class Cart extends \Phalcon\Di\Injectable
 
     /**
      * 删除物品
-     * @param $uid
      * @param $pid
      * @return bool
      */
-    public function Del($uid, $pid)
+    public function Del($pid)
     {
         $redis = $this->di->getShared('redis');
-        $key = "$this->Prefix:$uid:$pid";
+        $key = "$this->Prefix:$this->uid:$pid";
         //判断物品是否存在
         if ($redis->exists($key)) {
             $redis->del($key);
@@ -60,15 +70,14 @@ class Cart extends \Phalcon\Di\Injectable
 
     /**
      * 增加物品数量
-     * @param $uid
      * @param $pid
      * @param int $num
      * @return bool
      */
-    public function Increase($uid, $pid, $num = 1)
+    public function Increase($pid, $num = 1)
     {
         $redis = $this->di->getShared('redis');
-        $key = "$this->Prefix:$uid:$pid";
+        $key = "$this->Prefix:$this->uid:$pid";
         //判断购物车中是否存在此商品
         if ($redis->exists($key)) {
             $redis->hIncrBy($key, 'num', $num);
@@ -78,15 +87,14 @@ class Cart extends \Phalcon\Di\Injectable
 
     /**
      * 减少物品数量
-     * @param $uid
      * @param $pid
      * @param int $num
      * @return bool
      */
-    public function Decrease($uid, $pid, $num = 1)
+    public function Decrease($pid, $num = 1)
     {
         $redis = $this->di->getShared('redis');
-        $key = "$this->Prefix:$uid:$pid";
+        $key = "$this->Prefix:$this->uid:$pid";
         //判断购物车中是否存在此商品
         if ($redis->exists($key)) {
             //判断扣减的数量是否超过购物车的数量
@@ -101,13 +109,12 @@ class Cart extends \Phalcon\Di\Injectable
 
     /**
      * 显示购物车
-     * @param $uid
      * @return array
      */
-    public function cartList($uid)
+    public function cartList()
     {
         $redis = $this->di->getShared('redis');
-        $key = "$this->Prefix:$uid:*";
+        $key = "$this->Prefix:$this->uid:*";
         $cartKeyList = $redis->keys($key);
         $cart = array();
         foreach($cartKeyList as $item){
