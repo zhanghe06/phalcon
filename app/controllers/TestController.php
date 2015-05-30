5834f2a355b228bd8864a5ef991355bdcefa9e09
Zhang He <zhang_he06@163.com>
2015-05-30 23:58:52 +0800

    利用Model修改数据的简单应用

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -110,5 +110,26 @@ class TestController extends \Phalcon\Mvc\Controller
 
         die;
     }
+
+    public function userAction()
+    {
+        $user = new User();
+
+        //Store and check for errors
+        //$success = $user->save($this->request->getPost(), array('name', 'email'));
+        $user->name = '测试';
+        $success = $user->save();
+
+        if ($success) {
+            echo "Thanks for registering!";
+        } else {
+            echo "Sorry, the following problems were generated: ";
+            foreach ($user->getMessages() as $message) {
+                echo $message->getMessage(), "<br/>";
+            }
+        }
+
+        $this->view->disable();
+    }
 }
 
